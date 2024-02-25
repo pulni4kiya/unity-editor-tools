6f14a0d45ffeec31218aa13a517b0048b0014ee2
Pulni <pulni@pulni.com>
2024-02-25 19:34:12 +0200

    Allow groups in ExposedProperties

--- Editor/ExposedProperties/ExposedPropertiesEditor.cs
+++ Editor/ExposedProperties/ExposedPropertiesEditor.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,6 +8,8 @@ namespace Pulni.EditorTools.Editor {
 	[CustomEditor(typeof(ExposedProperties))]
 	public class ExposedPropertiesEditor : UnityEditor.Editor {
 		private bool isInEditMode = false;
+
+		private GroupNode rootNode;
 
 		public override void OnInspectorGUI() {
 			if (this.isInEditMode) {
@@ -25,25 +28,104 @@ namespace Pulni.EditorTools.Editor {
 			var exposedProperties = (ExposedProperties)this.target;
 			var config = exposedProperties.ExposedPropertiesConfig;
 
-			foreach (var param in config.Properties) {
-				if (param.Target == null) continue;
+			this.rootNode = new GroupNode("");
+			foreach (var property in config.Properties) {
+				this.rootNode.AddProperty(property);
+			}
 
-				var obj = new SerializedObject(param.Target);
-				var prop = obj.FindProperty(param.PropertyPath);
+			this.rootNode.ShowRootNodes();
+		}
 
-				if (prop == null) continue;
+		private interface INode {
+			public void ShowNode();
+		}
 
-				if (param.IsReadOnly || config.IsReadOnly) {
+		private class GroupNode : INode {
+			private List<INode> subnodes = new List<INode>();
+			private bool IsExpanded {
+				get {
+					return SessionState.GetBool($"{this.Name}_IsExpanded", false);
+				}
+				set {
+					SessionState.SetBool($"{this.Name}_IsExpanded", value);
+				}
+			}
+
+			public string Name { get; private set; }
+
+			public GroupNode(string name) {
+				this.Name = name;
+			}
+
+			public void ShowRootNodes() {
+				ShowNode(true);
+			}
+
+			public void ShowNode() {
+				ShowNode(false);
+			}
+
+			public void ShowNode(bool isRoot) {
+				if (!isRoot) {
+					this.IsExpanded = EditorGUILayout.Foldout(this.IsExpanded, this.Name);
+					EditorGUI.indentLevel++;
+				}
+				if (isRoot || this.IsExpanded) {
+					foreach (var node in subnodes) {
+						node.ShowNode();
+					}
+				}
+				if (!isRoot) {
+					EditorGUI.indentLevel--;
+				}
+			}
+
+			public void AddProperty(ExposedProperties.Param property) {
+				this.AddProperty(property, property.Label.Split('/'), 0);
+			}
+
+			private void AddProperty(ExposedProperties.Param property, string[] path, int indexInPath) {
+				if (indexInPath == path.Length - 1) {
+					this.subnodes.Add(new PropertyNode(property, path[indexInPath]));
+				} else {
+					var group = subnodes.OfType<GroupNode>().Where(grp => grp.Name == path[indexInPath]).FirstOrDefault();
+					if (group == null) {
+						group = new GroupNode(path[indexInPath]);
+						this.subnodes.Add(group);
+					}
+					group.AddProperty(property, path, indexInPath + 1);
+				}
+			}
+		}
+
+		private class PropertyNode : INode {
+			private ExposedProperties.Param property;
+			private string label;
+
+			public PropertyNode(ExposedProperties.Param property, string label) {
+				this.property = property;
+				this.label = label;
+			}
+
+			public void ShowNode() {
+				if (this.property.Target == null) return;
+
+				var obj = new SerializedObject(this.property.Target);
+				var prop = obj.FindProperty(this.property.PropertyPath);
+
+				if (prop == null) return;
+
+				if (this.property.IsReadOnly || this.property.IsReadOnly) {
 					GUI.enabled = false;
 				}
 
-				if (string.IsNullOrEmpty(param.Label)) {
-					EditorGUILayout.PropertyField(prop, param.ShowChildren);
+				if (string.IsNullOrEmpty(this.property.Label)) {
+					EditorGUILayout.PropertyField(prop, this.property.ShowChildren);
 				} else {
-					EditorGUILayout.PropertyField(prop, new GUIContent(param.Label), param.ShowChildren);
+					EditorGUILayout.PropertyField(prop, new GUIContent(this.label), this.property.ShowChildren);
 				}
 
-				if (param.IsReadOnly || config.IsReadOnly) {
+				if (this.property.IsReadOnly || this.property.IsReadOnly) {
 					GUI.enabled = true;
 				}
 
